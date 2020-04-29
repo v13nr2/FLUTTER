1329fc33df45021643adbb8b3f7bd6355e242051
v13nr <nanang.manajer@gmail.com>
2020-04-29 16:29:03 +0800

    fetching data

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,22 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+
+class Photo {
+  final String title;
+  final String thumbnailUrl;Photo._({this.title, this.thumbnailUrl});factory Photo.fromJson(Map<String, dynamic> json) {
+    return new Photo._(
+      title: json['title'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+}
+
 
 class Search extends StatefulWidget {
   @override
@@ -6,10 +24,61 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  List<Photo> list = List();
+  var isLoading = false;
+
+  _fetchData() async {
+    setState(() {
+      isLoading = true;
+    });
+    final response =
+        await http.get("https://jsonplaceholder.typicode.com/photos");
+    if (response.statusCode == 200) {
+      list = (json.decode(response.body) as List)
+          .map((data) => new Photo.fromJson(data))
+          .toList();
+      setState(() {
+        isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load photos');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fetch Data JSON"),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            child: new Text("Fetch Data"),
+            onPressed: _fetchData,
+          ),
+        ),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    title: new Text(list[index].title),
+                    trailing: new Image.network(
+                      list[index].thumbnailUrl,
+                      fit: BoxFit.cover,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                  );
+                }));
+  
   }
+  
 }
 
 class UserResult extends StatelessWidget {
---
+++ lib/utils.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+const String url_api = "http://10.0.2.2/sosmed/crud/service.php";
+
+class UserData {
+  static String userName = "";
+  static String userFullName = "";
+  static String userSession = "";
+}
+
+
+
+void setConfigUser(String _user) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('c_user', _user);
+}
+
+Future<String> getConfigUser() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String _data = prefs.getString('c_user');
+  return _data;
+}
+
+void setConfigSession(String _user) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('c_session', _user);
+}
+
+Future<String> getConfigSession() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String _data = prefs.getString('c_session');
+  return _data;
+}
+
+
+void showLoading(context, b) async {
+  if (b)
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Loading..."),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  else
+    Navigator.pop(context);
+}
+
+Future<bool> showAlert(BuildContext context, String text, String title) {
+  return showDialog<bool>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(text),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
+          FlatButton(
+            child: Text('Cancel'),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+Future httpPost(String url, Map<String, String> params) async {
+
+  try{
+    final response =
+    await http.post(url, body: params);
+
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return response.body;
+    } else {
+      // If that call was not successful, throw an error.
+      //throw Exception('Failed to load post');
+      return null;
+    }
+  }catch(e){
+    return null;
+  }
+
+}
