df6c56b3020c3bdd6470ede5da7e8d8044b4f3d1
v13nr <nanang.manajer@gmail.com>
2020-06-02 21:51:43 +0800

    upload Camra

--- lib/pages/input_from_cam.dart
+++ lib/pages/input_from_cam.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+//import 'package:sosmed/utils.dart' as utils;
+import 'package:http_parser/http_parser.dart';
 
 class InputCamera extends StatefulWidget {
   @override
@@ -10,41 +13,73 @@ class InputCamera extends StatefulWidget {
 class _InputCameraState extends State<InputCamera> {
   File _image;
   final imagePicker = ImagePicker();
+  Dio dio = Dio();
 
-  Future getImageCam() async {
+  Future<File> getImageCam() async {
     var image = await imagePicker.getImage(source: ImageSource.camera);
 
-    setState(() {
-      _image = File(image.path);
-    });
+    return File(image.path);
+  }
+
+  Future uploadForm() async {
+    final String url = "https://doktorsiaga.co.id/api/bukti_upload/add";
+
+    String fileName = _image.path.split('/').last;
+
+    try {
+      FormData formData = FormData.fromMap({
+        'photo': await MultipartFile.fromFile(
+          _image.path,
+          filename: fileName,
+          contentType: MediaType(
+            'picture',
+            'jpg',
+           ),
+         ),
+        'name': fileName.substring(0,3)+'.jpg',
+        //'photo': fileName+'.jpg',
+      });
+      //import utilsnya mas, td foto emg gak masuk ya?
+      var response = await dio.post(url,
+          data: formData,
+          //'x-api-key': utils.x_api_key,
+          options: Options(headers: {'x-api-key': 'DF1E02B621FBFD5849C54451D13BE778'}));
+      //print response dari server
+      print(response.data);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
+    //var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text('Input Camera', style:TextStyle(color:Colors.black54)),
+        title: Text('Input Camera', style: TextStyle(color: Colors.black54)),
         actions: [
-          
-          ///https://doktorsiaga.co.id/api/bukti_upload/add
-          ///
-          ///tipenya get atau post?
-          ///post
-          ///ada variabel yg di post?
-          ///1. header x-api-key = DF1E02B621FBFD5849C54451D13BE778
-          ///2. form photo (text)
-       
-          //oke saya ubah apinya dulu, file juga gpp
+          //https://doktorsiaga.co.id/api/bukti_upload/add
+          //variabel = photo(text), name(text)
+          //header x-api-key
+          // saya lanjut?
 
           GestureDetector(
             onTap: () {
-              getImageCam();
+  
+              getImageCam().then((value) {
+                setState(() {
+                  _image = value;
+                });
+               uploadForm().then((value) => print('uploaded'));
+              });
             },
             child: Padding(
-              padding: EdgeInsets.only(right:10),
-              child: Icon(Icons.camera, color: Colors.black54,)),
+                padding: EdgeInsets.only(right: 10),
+                child: Icon(
+                  Icons.camera,
+                  color: Colors.black54,
+                )),
           )
         ],
       ),
